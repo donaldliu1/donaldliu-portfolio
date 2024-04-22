be7ffdaeb7928e269bc6e22283fd8259cfaf0c5d
donaldliu1 <donaldliu1313@gmail.com>
2024-04-22 22:00:52 +0100

    changed symbol due to building error

--- src/app/portfolio/info.tsx
+++ src/app/portfolio/info.tsx
@@ -2,7 +2,7 @@ export default function Info() {
     return (
         <>
             <div className="border-solid border-2 border-black rounded-md bg-green-200 m-2.5">
-                <p className="text-xl"> I am a passionate and driven Junior Software Developer looking to challenge myself within the tech industry. I've always had an aptitude for coding, studying Software Development in college and coding in Python while studying physics at university. On the side I create and develop personal projects that pique my interest, furthering my learning and developing my skills. Effective at both independent problem-solving and collaborative teamwork, I bring strong communication skills to the table. Aspiring to contribute fresh perspectives and innovative solutions to any dynamic tech team. </p>
+                <p className="text-xl"> I am a passionate and driven Junior Software Developer looking to challenge myself within the tech industry. I&apos;ve always had an aptitude for coding, studying Software Development in college and coding in Python while studying physics at university. On the side I create and develop personal projects that pique my interest, furthering my learning and developing my skills. Effective at both independent problem-solving and collaborative teamwork, I bring strong communication skills to the table. Aspiring to contribute fresh perspectives and innovative solutions to any dynamic tech team. </p>
             </div>
         </>
     )
