dddf66aa0a4cba5e979fff42878de3d619114f67
donaldliu1 <donaldliu1313@gmail.com>
2024-04-24 14:05:57 +0100

    nav bar improvements

--- src/app/portfolio/navBar.tsx
+++ src/app/portfolio/navBar.tsx
@@ -1,7 +1,22 @@
 export default function NavBar() {
     return (
         <>
-        <div className="h-[45px] w-full bg-red-400">HI</div>
+        <div className="h-[45px] w-full bg-red-400">
+            <div className="flex flex-wrap justify-center text-center">
+                <div className="transform transition duration-500 hover:scale-110 p-2 text-white">
+                    <p>Portfolio</p>
+                </div>
+                <div className="transform transition duration-500 hover:scale-110 p-2 text-white">
+                    <p>Push & Pull</p>
+                </div>
+                <div className="transform transition duration-500 hover:scale-110 p-2 text-white">
+                    <p>Guesser</p>
+                </div>
+                <div className="transform transition duration-500 hover:scale-110 p-2 text-white">
+                    <p>Mini-projects</p>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
